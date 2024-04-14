46aec10aa9d4ebc84526721aa02cb8e45e4f9e61
MOHAMMED AYUBALI <108898063+realayub@users.noreply.github.com>
2024-04-14 15:35:55 +0530

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,12 +2,14 @@
 
 #include <iostream>
 using namespace std;
+int flag;
 
 void bubbleSort(int array[], int size)
 {
 
     for (int i = 0; i < size - 1; i++)
     {
+        flag=0;
         for (int j = 0; j < size - i - 1; j++)
         {
 
@@ -18,7 +20,11 @@ void bubbleSort(int array[], int size)
                 int temp = array[j];  
                 array[j] = array[j+1];
                 array[j + 1] = temp;
+                flag=1;
             }
+        }
+        if (flag==0){
+            break;
         }
     }
 }
