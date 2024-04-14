ab6df1b10404813573a8092ece5f72fd0731f858
MOHAMMED AYUBALI <108898063+realayub@users.noreply.github.com>
2024-04-14 02:58:56 -0700

    Add files via upload

---
+++ bubbleSort.cpp
@@ -0,0 +1,45 @@
+// Bubble sort in cpp
+
+#include <iostream>
+using namespace std;
+
+void bubbleSort(int array[], int size)
+{
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+
+            // comparing two adjacent elements
+            if (array[j] > array[j + 1])
+            {
+                // swapping elements if not in the order
+                int temp = array[j];  
+                array[j] = array[j+1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+//printing array
+
+void printArray(int array[],int size){
+    for(int j=0;j<size;j++){
+        cout<<"\n"<<array[j]; 
+    }
+    cout<<"\n";
+}
+
+int main() {
+  int data[] = {-2, 45, 0, 11, -9};
+  
+  // find array's length
+  int size = 5;
+  
+  bubbleSort(data, size);
+  
+  cout << "Sorted Array in Ascending Order:\n";  
+  printArray(data, size);
+}
